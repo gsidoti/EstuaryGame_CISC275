f4966017135be0c37a3ba1071b0784d22efba148
Gary Sidoti <gtsidoti@gmail.com>
2016-04-18 01:19:51 -0400

    made some changes, not really sure how to go about correctly setting up the view and controller separately.

--- src/OverallGame/Window.java
+++ src/OverallGame/Window.java
@@ -9,13 +9,18 @@ public class Window extends Canvas{
 
 	private static final long serialVersionUID = -5596174652453925475L;
 
-	public Window(int width, int height, String title){
+	public Window(int width, int height, String title, Game game){
 		JFrame frame = new JFrame(title);
 		frame.setPreferredSize(new Dimension(width,height));
 		frame.setMaximumSize(new Dimension(width,height));
 		frame.setMinimumSize(new Dimension(width,height));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.add(game);
+		frame.setVisible(true);
+		game.start();
 		
 	}
 	
