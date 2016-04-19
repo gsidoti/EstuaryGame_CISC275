c5d806c5ac48b117a69ea70d0b6087dbff3c2d12
Cody Fox <velithus@udel.edu>
2016-04-19 11:53:58 -0400

    changed MenuView to say game*

--- src/OverallGame/MenuView.java
+++ src/OverallGame/MenuView.java
@@ -20,25 +20,25 @@ public class MenuView{
 		g.fillRect((WIDTH/100)*20,(HEIGHT/100)*20,200,200);
 		g.setColor(Color.black);
 		g.setFont(new Font("TimesRoman", Font.PLAIN, 30));
-		g.drawString("START!",(WIDTH/100)*20+30,(HEIGHT/100)*20+80);
+		g.drawString("Game1",(WIDTH/100)*20+30,(HEIGHT/100)*20+80);
 		//second button
 		g.setColor(Color.red);
 		g.fillRect((WIDTH/100)*60,(HEIGHT/100)*20,200,200);
 		g.setColor(Color.black);
 		g.setFont(new Font("TimesRoman", Font.PLAIN, 30));
-		g.drawString("START!",(WIDTH/100)*60+30,(HEIGHT/100)*20+80);
+		g.drawString("Game2",(WIDTH/100)*60+30,(HEIGHT/100)*20+80);
 		
 		g.setColor(Color.red);
 		g.fillRect((WIDTH/100)*20,(HEIGHT/100)*60,200,200);
 		g.setColor(Color.black);
 		g.setFont(new Font("TimesRoman", Font.PLAIN, 30));
-		g.drawString("START!",(WIDTH/100)*20+30,(HEIGHT/100)*60+80);
+		g.drawString("Game3",(WIDTH/100)*20+30,(HEIGHT/100)*60+80);
 		
 		g.setColor(Color.red);
 		g.fillRect((WIDTH/100)*60,(HEIGHT/100)*60,200,200);
 		g.setColor(Color.black);
 		g.setFont(new Font("TimesRoman", Font.PLAIN, 30));
-		g.drawString("START!",(WIDTH/100)*60+30,(HEIGHT/100)*60+80);
+		g.drawString("Game4",(WIDTH/100)*60+30,(HEIGHT/100)*60+80);
 		
 		g.setColor(Color.green);
 		g.fillRect((WIDTH/100)*50-250,(HEIGHT/100)*2,500,70);
